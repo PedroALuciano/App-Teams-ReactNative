7127dc44aed462d50829e86fb7dacd89ae917c15
Pedro Luciano de Azevedo <pedro.azevedo@tote.local>
2024-03-26 16:46:20 -0300

    App complete

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import theme from '../theme';
+
+import 'styled-components/native';
+
+declare module 'styled-components/native' {
+  type ThemeType = typeof theme;
+  
+  export interface DefaultTheme extends ThemeType {}
+}
